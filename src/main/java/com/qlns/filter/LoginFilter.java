002c96c5c1b22f065709da816076c3834a85ff78
2003thanhquy <quythanhhuynh2003@gmail.com>
2023-12-10 02:40:46 +0700

    filter login

--- src/main/java/com/qlns/filter/LoginFilter.java
+++ src/main/java/com/qlns/filter/LoginFilter.java
@@ -1,22 +1,44 @@
 package com.qlns.filter;
 
+import com.qlns.model.TaiKhoan;
+
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.logging.LogRecord;
+import java.util.Collection;
+import java.util.Map;
 
+@WebFilter("/*")
 public class LoginFilter implements Filter{
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        System.out.print("day la filter ok");
-        Filter.super.init(filterConfig);
+
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
 
-        filterChain.doFilter(servletRequest, servletResponse);
+        String requestPath = request.getServletPath();
+        if (!(requestPath.equals("/login")||requestPath.equals("/login/login.jsp"))) {
+            HttpSession session = request.getSession();
+            TaiKhoan acc =(TaiKhoan)session.getAttribute("account");
+            if(acc==null){
+                response.sendRedirect(request.getContextPath()+"/login/login.jsp");
+                return;
+            }
+
+        }
+        filterChain.doFilter(request, response);
+
+
+
     }
 
     @Override
