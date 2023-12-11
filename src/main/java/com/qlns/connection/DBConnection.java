a27a5336b257ceebb37ff3bffb12f554b76dd949
PhamHuuTuan21110713 <phamhuutuan20031@gmail.com>
2023-12-11 19:22:10 +0700

    form danh sach du an

--- src/main/java/com/qlns/connection/DBConnection.java
+++ src/main/java/com/qlns/connection/DBConnection.java
@@ -9,7 +9,7 @@ public class DBConnection {
         String jdbcURL = "jdbc:mysql://localhost:3306/quanlynhansu?useSSL=false";
         String jdbcUsername = "root";
 
-        String jdbcPassword = "Huutan2k3@";
+        String jdbcPassword = "PhamHuuTuan258654@";
 
         Connection connection = null;
         try {
